0bb4020b63d77c55831719fed43225cf4d0c6511
troyhe <46601555+troyhe@users.noreply.github.com>
2020-12-25 21:16:19 -0500

    Create 0027_Remove Element.cpp

---
+++ 0027_Remove Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // length of the new array;
+        int len = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != val){
+                nums[len++] = nums[i];
+            }
+        }
+        return len;
+    }
+};
