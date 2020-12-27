4b2eb120173082b83ce92637f4cf45586ab1606d
troyhe <46601555+troyhe@users.noreply.github.com>
2020-12-26 19:39:26 -0500

    Create 0041_First Missing Positive.cpp

---
+++ 0041_First Missing Positive.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (int i = 0; i < nums.size();){
+            if (nums[i] <= 0 || nums[i] > nums.size()){
+                nums[i] = 0;
+                i++;
+            } else {
+                if (nums[i] != nums[nums[i]-1]) swap(nums[i],nums[nums[i]-1]);
+                else i++;
+            }
+        }
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return nums.size()+1;
+    }
+};
