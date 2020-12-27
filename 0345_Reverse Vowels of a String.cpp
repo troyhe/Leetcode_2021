c0ccfdbd068e9cb9f398d4f101e726c226d4bac5
troyhe <46601555+troyhe@users.noreply.github.com>
2020-12-27 11:25:59 -0500

    Create 0345_Reverse Vowels of a String.cpp

---
+++ 0345_Reverse Vowels of a String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    bool isVowel(char c){
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c =='U') return true;
+        return false;
+    }
+public:
+    string reverseVowels(string s) {
+        if (s.empty()) return s;
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right){
+            while (left < right && !isVowel(s[left])) left++;
+            while (left < right && !isVowel(s[right])) right--;
+            if (left < right){
+                swap(s[left++], s[right--]);
+            }
+        }
+        return s;
+    }
+};
