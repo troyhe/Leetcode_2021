78c6cb012603e95f67891d8cae31c6856215ea59
troyhe <46601555+troyhe@users.noreply.github.com>
2021-01-04 12:38:02 -0500

    Create 0628_Maximum Product of Three Numbers.cpp

---
+++ 0628_Maximum Product of Three Numbers.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        return max(nums[size-1]*nums[size-2]*nums[size-3], nums[size-1]*nums[0]*nums[1]);
+    }
+};
