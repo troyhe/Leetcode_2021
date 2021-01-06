8116707d8e13e2ce4f2a1eae80483f8f303d2ff8
troyhe <46601555+troyhe@users.noreply.github.com>
2021-01-06 14:13:58 -0500

    Create 0516_Longest Palindromic Subsequence.cpp

---
+++ 0516_Longest Palindromic Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1 = text1.length() + 1;
+        int len2 = text2.length() + 1;
+        vector<vector<int>> dp(len1, vector<int>(len2));
+        for (int i = 0; i < len1; i++){
+            for (int j = 0; j < len2; j++){
+                if (i * j == 0) dp[i][j] = 0;
+                else if (text1[i-1] == text2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                } else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }    
+        }
+        return dp[len1-1][len2-1];
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+        return longestCommonSubsequence(s,t);
+    }
+};
