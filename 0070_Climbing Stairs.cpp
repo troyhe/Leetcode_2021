fcdba3351cc94a8b4bf1cc1b9a3972b5c6c77218
troyhe <46601555+troyhe@users.noreply.github.com>
2021-01-09 15:35:04 -0500

    Create 0070_Climbing Stairs.cpp

---
+++ 0070_Climbing Stairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,1);
+        for (int i = 2; i < n + 1; i++){
+            dp[i] = dp[i-2] + dp[i-1]; 
+        }
+        return dp[n];
+    }
+};
